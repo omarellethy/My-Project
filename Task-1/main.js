de32c567ea5febf06a4b3753241e6be71ef89b34
omarellethy <95581289+omarellethy@users.noreply.github.com>
2023-05-05 16:25:25 +0300

    Add files via upload

---
+++ Task-1/main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+function mobiles(mobile){
+    img.src = mobile;
+}
+function colors(color){
+    container.style.background = color;
+}
